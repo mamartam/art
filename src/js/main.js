98bdb383dce94d9aac7468e3d5066ea3b11631c9
mamartam <marta.masiukk@gmail.com>
2026-04-08 16:47:16 +0300

    add script for ariaExpanded attribute

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
-const headerNavNurger = document.querySelector(".header__nav--burger");
+const headerNavBurger = document.querySelector(".header__nav--burger");
 const body = document.querySelector(".body");
 
-headerNavNurger.addEventListener("click", (event) => {
+headerNavBurger.addEventListener("click", (event) => {
   if (event.currentTarget.classList.contains("active")) {
-    console.log("yes");
-    headerNavNurger.classList.remove("active");
-    headerNavNurger.classList.add("not-active");
+    headerNavBurger.ariaExpanded = "true";
+    headerNavBurger.classList.remove("active");
+    headerNavBurger.classList.add("not-active");
     body.classList.remove("activeBurger");
   } else if (event.currentTarget.classList.contains("not-active")) {
-    console.log("no");
-    headerNavNurger.classList.add("active");
-    headerNavNurger.classList.remove("not-active");
+    headerNavBurger.ariaExpanded = "false";
+    headerNavBurger.classList.add("active");
+    headerNavBurger.classList.remove("not-active");
     body.classList.add("activeBurger");
   }
 });
@@ -20,13 +20,15 @@ window.addEventListener("resize", () => {
 
   console.log(`Browser Viewport Size: ${windowWidth}`);
   if (windowWidth > 992) {
-    headerNavNurger.classList.remove("active");
-    headerNavNurger.classList.add("not-active");
+    headerNavBurger.ariaExpanded = "false";
+    headerNavBurger.classList.remove("active");
+    headerNavBurger.classList.add("not-active");
 
     body.classList.remove("activeBurger");
   } else {
-    headerNavNurger.classList.remove("active");
-    headerNavNurger.classList.add("not-active");
+    headerNavBurger.ariaExpanded = "false";
+    headerNavBurger.classList.remove("active");
+    headerNavBurger.classList.add("not-active");
 
     body.classList.remove("activeBurger");
   }
